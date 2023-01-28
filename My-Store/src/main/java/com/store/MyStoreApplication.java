f38b674ea883bc8cc86961a0028df26ee20c00e0
Amit-singh-05 <ami9sin05@gmail.com>
2023-01-28 12:18:41 +0530

    Basic spring boot application structure created for my store application

---
+++ My-Store/src/main/java/com/store/MyStoreApplication.java
@@ -0,0 +1,13 @@
+package com.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyStoreApplication.class, args);
+	}
+
+}
